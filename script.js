6ff8919e943c772918b793046fb7357cfbf63884
nisi <49096264+niallsiow@users.noreply.github.com>
2023-06-27 20:15:04 +0100

    Add functionality to display input

--- script.js
+++ script.js
@@ -19,10 +19,10 @@ let b = 0;
 let operator = null;
 
 const Operations = {
-    Add: "add",
-    Subtract: "subtract",
-    Multiply: "multiply",
-    Divide: "divide"
+    Add:        "add",
+    Subtract:   "subtract",
+    Multiply:   "multiply",
+    Divide:     "divide"
 }
 
 function operate(a, b, operator){
@@ -43,3 +43,40 @@ function operate(a, b, operator){
             console.log("Invalid Operand!");
     }
 }
+
+let displayValue = null;
+
+const display = document.querySelector('.display');
+
+function updateDisplay(input){
+    if(!displayValue){
+        displayValue = input;
+    }
+    else{
+        displayValue += input;
+    }
+
+    display.textContent = displayValue;
+}
+
+const button0 = document.querySelector('.button0');
+const button1 = document.querySelector('.button1');
+const button2 = document.querySelector('.button2');
+const button3 = document.querySelector('.button3');
+const button4 = document.querySelector('.button4');
+const button5 = document.querySelector('.button5');
+const button6 = document.querySelector('.button6');
+const button7 = document.querySelector('.button7');
+const button8 = document.querySelector('.button8');
+const button9 = document.querySelector('.button8');
+
+button0.addEventListener('click', () => updateDisplay('0'));
+button1.addEventListener('click', () => updateDisplay('1'));
+button2.addEventListener('click', () => updateDisplay('2'));
+button3.addEventListener('click', () => updateDisplay('3'));
+button4.addEventListener('click', () => updateDisplay('4'));
+button5.addEventListener('click', () => updateDisplay('5'));
+button6.addEventListener('click', () => updateDisplay('6'));
+button7.addEventListener('click', () => updateDisplay('7'));
+button8.addEventListener('click', () => updateDisplay('8'));
+button9.addEventListener('click', () => updateDisplay('9'));
